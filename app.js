35aaffee8308d2903a5fc19c8865bbd75326f03a
Nathan McDowell <nathan.mcdowell@vitals.com>
2014-04-30 23:29:55 -0400

    Working multiplayer bubbles

--- app.js
+++ app.js
@@ -8,19 +8,26 @@ app.use(express.static(__dirname + '/'));
 	
 // This is the port for the web server.
 // you will need to go to 10.10.10.100:80 to see it
-server.listen(80);
+server.listen(8080);
 
 io.set('log level', 1);
+
+var userID = 0;
 
 // Listen for incoming connections from clients
 io.sockets.on('connection', function (socket) {
 
+	// Assign and imcrement userID on connection
+	socket.emit('assignid',userID);
+	userID++;
+
 	// Start listening for mouse move events
 	socket.on('mousemove', function (data) {
 		
+		console.log(data);
 		// This line sends the event (broadcasts it)
 		// to everyone except the originating client.
 		io.sockets.emit('moving', data);
-		console.log(data);
+		//console.log(data);
 	});
 });
